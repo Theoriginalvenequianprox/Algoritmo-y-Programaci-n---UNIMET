a77e82b8929a1d7bb4fa9c7b1f240fa11d87dcf5
Gabriel Viñas <gvinas@correo.unimet.edu.ve>
2025-01-07 22:21:40 -0400

    Add files via upload

---
+++ Tarea1_Github.py
@@ -0,0 +1,7 @@
+#Hola profesor Torres, esta es una prueba de las funciones "print" y "output"
+
+name = input('Cuál es tú nombre? ')
+
+print("Hola", name)
+
+input('Presione cualquier tecla para continuar ')
